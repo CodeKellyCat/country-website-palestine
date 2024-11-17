04ad218f609d1b2fec737ade3627fce5261252f5
Kelly McLaughlin <kmclaughlin9@my.bcit.ca>
2024-11-16 19:37:16 -0800

    formatted for desktop with display grid and flexbox for mobile.

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,15 +1,8 @@
 // Main Navigation Toggle
-const hamburger = document.getElementById("nav-hamburger");
-const links = document.getElementById("nav-links");
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
-button.addEventListener('click', function() {
-    links.classList.toggle('toggled');
-})
-
-// Thumb Navigation Toggle
-const thumbButton = document.getElementById("thumb-nav-button");
-const thumbLinks = document.getElementById("thumb-nav-secondary");
-
-thumbButton.addEventListener('click', function() {
-    thumbLinks.classList.toggle("toggled");
-})
+menuToggle.addEventListener("click", function() {
+    navLinks.classList.toggle("toggled");
+    console.log("click");
+});
